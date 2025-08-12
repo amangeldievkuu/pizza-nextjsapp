b8361cdeb9c3b1ad546e661838603b5f33dec5e2
amangeldievkuu <amangeldievkuu@gmail.com>
2025-08-12 16:49:26 +0700

    custom nav-links with SSR

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
-import logoImg from "@/assets/logo.png";
-import classes from "./main-header.module.css";
 import Image from "next/image";
+
 import MainHeaderBackground from "./main-header-background";
+import classes from "./main-header.module.css";
+import logoImg from "@/assets/logo.png";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   return (
@@ -16,13 +18,11 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Meals</Link>
+              <NavLink href="/meals">Meals</NavLink>
             </li>
+
             <li>
-              <Link href="/community">Community</Link>
-            </li>
-            <li>
-              <Link href="/meals/share">Share a Meal</Link>
+              <NavLink href="/community">Community</NavLink>
             </li>
           </ul>
         </nav>
